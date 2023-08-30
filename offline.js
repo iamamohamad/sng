233ebfab9daffdcc30302c120df8eba3891ec0de
iamamohamad <mmdmohamad86@gmail.com>
2023-08-30 22:12:18 +0430

    singup

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1693417310,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/logo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"SweetAlert.css",
+		"SweetAlert.js"
+	]
+}
